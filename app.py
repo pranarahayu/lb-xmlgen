81f42e766d911ae84522ad4f71983a2c5e2ac5d7
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-01-31 10:42:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,19 +21,19 @@ with col1:
     tl_data = st.file_uploader("Upload file timeline excel!")
     try:
         tl = pd.read_excel(tl_data, skiprows=[0])
+        c_data = datacleaner(tl)
+        st.write(c_data)
+
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            c_data.to_excel(writer, sheet_name='Sheet1', index=False)
+            download2 = st.download_button(
+                label="Download data as Excel",
+                data=buffer,
+                file_name='file-excel.xlsx',
+                mime='application/vnd.ms-excel')
+            
     except ValueError:
         st.error("Please upload the timeline file")
-
-    c_data = datacleaner(tl)
-    st.write(c_data)
-
-    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-        df.to_excel(writer, sheet_name='Sheet1', index=False)
-        download2 = st.download_button(
-            label="Download data as Excel",
-            data=buffer,
-            file_name='file-excel.xlsx',
-            mime='application/vnd.ms-excel')
 
 '''
 pn = plot_PN(pass_between, min_pass, filter, menit[0], menit[1], match, gw)
