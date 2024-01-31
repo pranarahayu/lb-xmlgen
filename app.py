5f9d37bd72f5a61a9c3b2fbe0a542e54236a19b3
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-01-31 11:07:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import numpy as np
 import streamlit as st
 import io
 
+import openpyxl, yattag
+from openpyxl import load_workbook
+from yattag import Doc, indent
+
 st.set_page_config(page_title='Lapangbola XML Generator for Video Tagging', layout='wide')
 st.header('XML File Generator')
 st.markdown('Created by: Prana - R&D Division Lapangbola.com')
@@ -28,17 +32,61 @@ with col1:
         download = st.download_button(
             label="Download data as Excel",
             data=buffer.getvalue(),
-            file_name='file-excel.xlsx',
+            file_name='clean-data.xlsx',
             mime='application/vnd.ms-excel')
             
     except ValueError:
         st.error("Please upload the timeline file")
 
-'''
-pn = plot_PN(pass_between, min_pass, filter, menit[0], menit[1], match, gw)
-st.pyplot(pn)
+with col2:
+    cl_data = st.file_uploader("Upload file clean-data.xlsx!")
+    try:
+        wb = load_workbook(cl_data)
+        ws = wb.worksheets[0]
+        doc, tag, text = Doc().tagtext()
 
-with open('pnet.jpg', 'rb') as img:
-    fn = 'PN_'+filter+'.jpg'
-    btn = st.download_button(label="Download Passing Network", data=img, file_name=fn, mime="image/jpg")
-'''
+        xml_header = ''
+        xml_schema = ''
+
+        doc.asis(xml_header)
+        doc.asis(xml_schema)
+
+        with tag('file'):
+          with tag('ALL_INSTANCES'):
+            for row in ws.iter_rows(min_row=2, max_row=1000, min_col=1, max_col=12):
+                row = [cell.value for cell in row]
+                with tag("instance"):
+                    with tag("ID"):
+                        text(row[0])
+                    with tag("code"):
+                        text(row[1])
+                    with tag("start"):
+                        text(row[2])
+                    with tag("end"):
+                        text(row[3])
+                    with tag('label'):
+                      with tag('group'):
+                        text(row[4])
+                      with tag('text'):
+                        text(row[5])
+                    with tag('label'):
+                      with tag('group'):
+                        text(row[6])
+                      with tag('text'):
+                        text(row[7])
+                    with tag('label'):
+                      with tag('group'):
+                        text(row[8])
+                      with tag('text'):
+                        text(row[9])
+
+        result = indent(doc.getvalue(), indentation = '    ',indent_text = True)
+        with open("xml-data.xml", "w") as f:
+            download1 = st.download_button(
+                label="Download XML data",
+                data=result,
+                file_name='xml-data.xml',
+                mime='text/csv')
+
+    except ValueError:
+        st.error("Please upload the clean-data")
